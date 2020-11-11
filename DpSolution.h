a12afa37fe9fd8f3d91b4d5f8023344ff395ea50
r000setta <1509248091@qq.com>
2020-11-11 20:48:25 +0800

    update

--- DpSolution.h
+++ DpSolution.h
@@ -205,8 +205,52 @@ public:
         return dp[m][n];
     }
 
-    int minDistance(string word1, string word2) {
+    int cuttingRope(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        vector<unsigned long long> dp(n + 1);
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for (unsigned long long i = 4; i <= n; ++i) {
+            for (int j = 1; j < i; ++j) {
+                dp[i] = max(i, max(dp[i], j * dp[i - j]));
+            }
+        }
+        return dp[n]%1000000007 == 1000000008 ? -1 : dp[n]%1000000007;
+    }
 
+    int minDistance(string word1, string word2) {
+        int m = word1.size(), n = word2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        for (int i = 0; i <= m; ++i) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i < n; ++i) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (i)
+            }
+        }
+    }
+
+    int maxDotProduct(vector<int> &nums1, vector<int> &nums2) {
+        int m = nums1.size(), n = nums2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MIN));
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                dp[i][j] = max(dp[i][j], nums1[i - 1] * nums2[j - 1]);
+                dp[i][j] = max(dp[i][j], nums1[i - 1] * nums2[j - 1] + dp[i - 1][j - 1]);
+                dp[i][j] = max(dp[i][j], dp[i][j - 1]);
+                dp[i][j] = max(dp[i][j], dp[i - 1][j]);
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - 1]);
+            }
+        }
+        return dp[m][n];
     }
 };
 
