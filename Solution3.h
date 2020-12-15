a015a60fd3f7e72a72802c9a4db0959f97b284df
r000setta <1509248091@qq.com>
2020-12-15 22:44:43 +0800

    update

--- Solution3.h
+++ Solution3.h
@@ -484,8 +484,8 @@ public:
         ListNode *cur = head;
         int ans = 0;
         while (cur != nullptr) {
-            ans=ans*2+cur->val;
-            cur=cur->next;
+            ans = ans * 2 + cur->val;
+            cur = cur->next;
         }
         return ans;
     }
@@ -502,6 +502,24 @@ public:
             ans.emplace_back(it->second);
         }
         return ans;
+    }
+
+    int monotoneIncreasingDigits(int N) {
+        string strN = to_string(N);
+        int i = 1;
+        while (i < strN.length() && strN[i - 1] <= strN[i]) {
+            i += 1;
+        }
+        if (i < strN.length()) {
+            while (i > 0 && strN[i - 1] > strN[i]) {
+                strN[i - 1] -= 1;
+                i -= 1;
+            }
+            for (i += 1; i < strN.length(); ++i) {
+                strN[i] = '9';
+            }
+        }
+        return stoi(strN);
     }
 };
 
